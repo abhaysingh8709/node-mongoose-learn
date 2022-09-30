f472f11c5660c51ade6833c08fc27a36ab7b20c8
abhay <abhay@pitangent.com>
2022-09-30 16:33:16 +0530

    add changes

--- Routes/route-config.js
+++ Routes/route-config.js
@@ -4,8 +4,9 @@ const app = express()
 app.use(express.json());
 
 const user_routes = require('./UserRouter.js')
-
+const auth_routes = require('./AuthRouter.js')
 
 app.use('/user', user_routes)
+app.use('/auth', auth_routes)
 
 app.listen(4500);
--- Schemas/UserSchema.js
+++ Schemas/UserSchema.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
-    name:String,
-    email:String,
-    price:Number
+    name: String,
+    email: String,
+    price: Number,
+    password: String,
 });
 
 module.exports = userSchema;
