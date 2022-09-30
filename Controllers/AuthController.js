5e433526da7d53c1827884b927739a1b44538bbc
abhay <abhay@pitangent.com>
2022-09-30 17:04:59 +0530

    add changes

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,15 +1,31 @@
 const User = require('../Models/UserModel');
 const cors = require('cors');
 
+//FOR LOGIN PAGE SHOW
 const showLogin = (async (req, resp) => {
     resp.send(req.body);
 });
+
+// FOR LOGIN
 const login = (async (req, resp) => {
-    resp.send(req.body);
+    if (req.body.email && req.body.password) {
+        let user = await User.findOne(req.body).select("-password");
+        if (user) {
+            resp.send(user);
+        } else {
+            resp.send({ result: "No User Found" });
+        }
+    } else {
+        resp.send({ result: "Please fill data" });
+    }
 });
+
+//FOR REGISTER PAGE SHOW
 const showRegister = (async (req, resp) => {
     resp.send(req.body);
 });
+
+//FOR REGISTER
 const register = (async (req, resp) => {
     resp.send(req.body);
 });
